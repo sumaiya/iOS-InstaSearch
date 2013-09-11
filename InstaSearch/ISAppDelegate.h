9fe3f75d3409d5d3578b36935b152dcb6c0feea6
Sumaiya <sumaiyahashmi@gmail.com>
2013-09-11 14:56:20 -0700

    Initial Commit

---
+++ InstaSearch/ISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ISAppDelegate.h
+//  InstaSearch
+//
+//  Created by Sumaiya Hashmi on 9/11/13.
+//  Copyright (c) 2013 Sumaiya Hashmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstaSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstaSearch
+//
+//  Created by Sumaiya Hashmi on 9/11/13.
+//  Copyright (c) 2013 Sumaiya Hashmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ISAppDelegate class]));
+    }
+}
